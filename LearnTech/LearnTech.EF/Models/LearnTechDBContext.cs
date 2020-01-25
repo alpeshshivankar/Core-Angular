61c7fe599d0a3b985de2eebbc6f36878413a2fb1
Alpesh Shivankar <shivankar.alpesh@gmail.com>
2020-01-25 16:58:48 +0530

    Added new Project For Entity Framework

---
+++ LearnTech/LearnTech.EF/Models/LearnTechDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace LearnTech.EF.Models
+{
+    public class LearnTechDBContext : DbContext
+    {
+        public LearnTechDBContext(): base("name = LearnTechConnectionstring")
+        {
+            Database.SetInitializer<LearnTechDBContext>(new CreateDatabaseIfNotExists<LearnTechDBContext>());
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
---
+++ LearnTech/LearnTech.EF/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LearnTech.EF.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+
+        public Grade Grade { get; set; }
+    }
+}
